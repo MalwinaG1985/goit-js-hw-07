029498728a3cbf6473e9facccd9f57256fa6ac32
Malwina Grysiak <malwinagrysiak@gmail.com>
2023-02-27 23:13:05 +0100

    second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = galleryItems;
+const galleryList = document.querySelector('.gallery');
+
+
+const newGallery = gallery.map(({preview, original, description}) => {
+    return `<a class="gallery__item" href="${original}">
+  <img  src="${preview}" 
+  alt="${description}" 
+  class="gallery__image" 
+  />
+</a>`
+}).join('');
+
+galleryList.insertAdjacentHTML('beforeend', newGallery);
+
+const lighbox = new SimpleLightbox('.gallery a', { 'captionsData': 'alt', 'captionDelay': '250' });
+lighbox.on('show.SimpleLightbox');
